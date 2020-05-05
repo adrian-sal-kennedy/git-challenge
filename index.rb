afef34bba2286abb6997772fe14da055f0218496
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-05 16:33:56 +1000

    capital I

--- index.rb
+++ index.rb
@@ -9,7 +9,7 @@ def hello_world
     address: Faker::Address.full_address
   }
   puts "Hello world".colorize(color: :black, background: :light_white)
-  puts "I'm #{data[:name]} and i live at #{data[:address]}".colorize(color: :black, background: :light_white)
+  puts "I'm #{data[:name]} and I live at #{data[:address]}".colorize(color: :black, background: :light_white)
 end
 
 hello_world
