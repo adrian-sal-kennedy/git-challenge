92402b007d704e61f0538f82f61bc9db3f8dda2b
PING NGE <ping.nge@hotmail.com>
2020-05-05 16:22:42 +1000

    added faker gem

--- index.rb
+++ index.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts "hello world"
+require 'faker'
+
+
+
+def hello_world
+  data = {
+    name: Faker::Name.name,
+    address: Faker::Address.full_address
+  }
+  puts "Hello world"
+  puts "I'm #{data[:name]} and i live at #{data[:address]}"
+end
+
+hello_world
